5c6c0b362a2bf64d024e81442ae1ace9d9f7f50e
ess234 <ess234@naver.com>
2022-10-10 03:01:57 +0900

    feat(GameController): GameController 추가
    - GameController 플레이 메소드 구현
    - Application 에 컨트롤러 호출 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,38 @@
+package racingcar.controller;
+
+import racingcar.service.RacingCar;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class GameController {
+    RacingCar racingCar;
+
+    public void play() {
+        racingCar = new RacingCar(inputCarsName());
+
+        racingCar.start(inputTryCnt());
+        racingCar.end();
+    }
+
+    private int inputTryCnt() {
+        int tryCnt;
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        try {
+            tryCnt = Integer.parseInt(readLine());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] TryCnt is not Number");
+        }
+
+        return tryCnt;
+    }
+
+    private String inputCarsName() {
+        String carsName;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        carsName = readLine();
+
+        return carsName;
+    }
+}
