ac3609e9ea9360a174b5d2e6be49b2f4b81b1302
ess234 <ess234@naver.com>
2022-10-10 02:59:44 +0900

    feat(RacingCar): RacingCar 기능 추가 및 로직 개선
    - 우승 자동차 정보 출력 메소드 구현
    - ApplicationTest에 맞춰 Cars 생성 및 기본 정보 세팅 로직 개선

--- src/main/java/racingcar/service/RacingCar.java
+++ src/main/java/racingcar/service/RacingCar.java
@@ -1,26 +1,62 @@
 package racingcar.service;
 
+import lombok.Getter;
+import racingcar.dto.Car;
+import racingcar.dto.Cars;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
 public class RacingCar {
-    int tryCnt = 0;
+    private static final String SEPARATOR_CARS_NAME = ",";
+    Cars cars;
 
-    public RacingCar(String tryCnt) {
-        validation(tryCnt);
-        this.tryCnt = Integer.parseInt(tryCnt);
+    public RacingCar(String carsName) {
+        this.validationCarsName(carsName);
+
+        this.cars = new Cars(Arrays.asList(carsName.split(SEPARATOR_CARS_NAME)));
     }
 
-    private void validation(String tryCnt) {
-        volidationIsNum(tryCnt);
-    }
-
-    private void volidationIsNum(String tryCnt) {
-        try {
-            Integer.parseInt(tryCnt);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] TryCnt is not Number");
+    private void validationCarsName(String carsNameStr) {
+        if (!carsNameStr.contains(SEPARATOR_CARS_NAME)) {
+            throw new IllegalArgumentException("[ERROR] cars name does not contain correct separator");
         }
     }
 
-    public int getTryCnt() {
-        return this.tryCnt;
+    public boolean start(int tryCnt) {
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < tryCnt; i++) {
+            this.cars.play();
+
+            System.out.println();
+        }
+
+        return true;
+    }
+
+    public boolean end() {
+        System.out.println("최종 우승자 : " + printWinnerCarsName());
+
+        return true;
+    }
+
+    private String printWinnerCarsName() {
+        String result = "";
+
+        List<Car> winnerCar = this.getWinnerCars();
+
+        for (Car car : winnerCar) {
+            result += car.getName() + ",";
+        }
+
+        return result.substring(0, result.length() - 1);
+    }
+
+    private List<Car> getWinnerCars() {
+        List<Car> winnerCars = this.cars.getCarsByMaxDistance();
+
+        return winnerCars;
     }
 }
